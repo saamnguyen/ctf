df8e444829baf358f976d5dc5a87c95377430d69
saamnguyen <nguyensam576@gmail.com>
2022-05-30 09:46:55 -0400

    fix Small Big Mistake

---
+++ writeups/heroctf_v4_2022/web/SmallBigMistake/test_hexdigits.py
@@ -0,0 +1,10 @@
+
+# import string library function 
+import string 
+from random import choice
+
+# Storing the value in variable result 
+result = choice(string.hexdigits ) * 32
+    
+# Printing the value 
+print(result) 
